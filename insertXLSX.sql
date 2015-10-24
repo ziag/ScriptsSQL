f455e579f77720329c912091a457a08acba83ea2
ziag <ziag@yahoo.com>
2015-10-24 05:58:05 -0400

    aaa

--- insertXLSX.sql
+++ insertXLSX.sql
@@ -19,6 +19,8 @@ Database=D:\tempSQL\fichier.xlsx'
 ,'SELECT * FROM [Feuil1$]'
 )
 
+
+
 /*
 INSERT INTO tblSample
 2.
@@ -27,5 +29,8 @@ SELECT * FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
 'Excel 12.0 Xml;HDR=YES;Database=C:\Test.xlsx',
 4.
 'SELECT * FROM [Sheet1$]');
+
+
+;.l,kmn jb
 */
  
