010359a03021b874571162617f34618d0d0d9aee
Simon <shuard@uda.ca>
2015-12-15 13:48:50 -0500

    Maj Create mail account

--- CreateDatabaseMailAccount.sql
+++ CreateDatabaseMailAccount.sql
@@ -1,3 +1,26 @@
+
+/*
+
+USE msdb
+GO
+
+EXEC sp_addrolemember 'DatabaseMailUserRole', 'un user'
+
+EXEC msdb.sys.sp_helprolemember 'DatabaseMailUserRole';
+
+EXEC msdb.dbo.sysmail_help_principalprofile_sp;
+EXEC msdb.dbo.sysmail_help_status_sp;
+
+EXEC msdb.dbo.sysmail_start_sp;
+EXEC msdb.dbo.sysmail_help_queue_sp @queue_type = 'mail';
+
+EXEC sp_send_dbmail @profile_name='Mail_Administrator', 
+			 @recipients='xxx@yahoo.com', 
+			 @body='test message', 
+			 @subject='Ignore this message'
+
+*/
+
 -- Create a Database Mail account
 EXECUTE msdb.dbo.sysmail_add_account_sp
     @account_name = 'SQL Administrator',
@@ -23,3 +46,5 @@ EXECUTE msdb.dbo.sysmail_add_profileaccount_sp
 --    @profile_name = 'SQL Administrator Profile',
 --    @principal_name = 'ApplicationUser',
 --    @is_default = 1 ;
+
+
