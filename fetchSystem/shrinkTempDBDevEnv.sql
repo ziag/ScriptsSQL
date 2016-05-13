59ec90805daad8f257e9fb29a836b24cc7b77c09
Simon <shuard@uda.ca>
2016-05-13 08:58:03 -0400

    troubleshooting TempDB

---
+++ fetchSystem/shrinkTempDBDevEnv.sql
@@ -0,0 +1,17 @@
+use tempdb
+GO
+
+DBCC FREEPROCCACHE -- clean cache
+DBCC DROPCLEANBUFFERS -- clean buffers
+DBCC FREESYSTEMCACHE ('ALL') -- clean system cache
+DBCC FREESESSIONCACHE -- clean session cache
+DBCC SHRINKDATABASE(tempdb, 10); -- shrink tempdb
+dbcc shrinkfile ('tempdev') -- shrink db file
+dbcc shrinkfile ('templog') -- shrink log file
+GO
+
+-- report the new file sizes
+SELECT name, size
+FROM sys.master_files
+WHERE database_id = DB_ID(N'tempdb');
+GO
