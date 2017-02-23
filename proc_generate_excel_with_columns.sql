d54711f601cca55239e9cbabd6a12445bdfe090e
Simon <shuard@uda.ca>
2017-02-23 07:57:59 -0500

    new file:   proc_generate_excel_with_columns.sql

---
+++ proc_generate_excel_with_columns.sql
@@ -0,0 +1,39 @@
+-- Stored Procedure
+
+CREATE procedure [dbo].[proc_generate_excel_with_columns]
+(
+	@db_name	varchar(100),
+	@table_name	varchar(100),	
+	@file_name	varchar(100)
+)
+as
+
+--Generate column names as a recordset
+declare @columns varchar(8000), @sql varchar(8000), @data_file varchar(100)
+select 
+	@columns=coalesce(@columns+',','')+column_name+' as '+column_name 
+from 
+	information_schema.columns
+where 
+	table_name=@table_name
+select @columns=''''''+replace(replace(@columns,' as ',''''' as '),',',',''''')
+
+--Create a dummy file to have actual data
+select @data_file=substring(@file_name,1,len(@file_name)-charindex('\',reverse(@file_name)))+'\data_file.xls'
+
+--Generate column names in the passed EXCEL file
+set @sql='exec master..xp_cmdshell ''bcp " select * from (select '+@columns+') as t" queryout "'+@file_name+'" -c -T -C ACP '''
+exec(@sql)
+
+--Generate data in the dummy file
+set @sql='exec master..xp_cmdshell ''bcp "select * from '+@db_name+'..'+@table_name+'" queryout "'+@data_file+'" -c -T -C ACP '''
+exec(@sql)
+
+--Copy dummy file to passed EXCEL file
+set @sql= 'exec master..xp_cmdshell ''type '+@data_file+' >> "'+@file_name+'"'''
+exec(@sql)
+
+--Delete dummy file 
+set @sql= 'exec master..xp_cmdshell ''del '+@data_file+''''
+exec(@sql)
+GO
