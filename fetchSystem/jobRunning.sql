fc40da0773949e7e17c58e46ce28df2ae12b9533
Simon <shuard@uda.ca>
2018-03-23 08:35:31 -0400

    renamed:    fetchSystem/CheckLastRunningDate.sql -> Job/CheckLastRunningDate.sql 	renamed:    fetchSystem/checkJobActivity.sql -> Job/checkJobActivity.sql 	renamed:    fetchSystem/checkJobSQL.sql -> Job/checkJobSQL.sql 	modified:   fetchSystem/jobRunning.sql

--- fetchSystem/jobRunning.sql
+++ fetchSystem/jobRunning.sql
@@ -1,10 +1,20 @@
 BEGIN TRAN
 
+
 SELECT sj.name
-   , sja.*
+      ,MAX( sja.last_executed_step_date ) lastDate 
+   
 FROM msdb.dbo.sysjobactivity AS sja
-INNER JOIN msdb.dbo.sysjobs AS sj ON sja.job_id = sj.job_id
+
+INNER JOIN msdb.dbo.sysjobs AS sj 
+	ON sja.job_id = sj.job_id
+
 WHERE sja.start_execution_date IS NOT NULL
-   AND sja.stop_execution_date IS NULL
+ 
+GROUP BY sj.name
+
+ORDER BY  MAX( sja.last_executed_step_date ) DESC
+
+
 
 ROLLBACK
