d4f41fe4a52091e001785a71ecf1a26e3a8e6207
Simon <shuard@uda.ca>
2018-07-17 09:35:12 -0400

    new file:   fetchSystem/memory_Used_By_Each_Database.sql

---
+++ fetchSystem/memory_Used_By_Each_Database.sql
@@ -0,0 +1,11 @@
+BEGIN TRAN
+
+-- Memory used by each database
+SELECT DB_NAME(database_id),
+COUNT (1) * 8 / 1024 AS MBUsed
+FROM sys.dm_os_buffer_descriptors
+GROUP BY database_id
+ORDER BY COUNT (*) * 8 / 1024 DESC
+GO
+
+ROLLBACK
