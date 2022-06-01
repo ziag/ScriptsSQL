2b90f22309e3be3f5f5891d52b4da033409ac4b1
Simon Huard <shuard@cpaquebec.ca>
2022-06-01 14:48:26 -0400

    Update fetchTableColonneProcedure.sql
    maj mineure

--- fetchSystem/fetchTableColonneProcedure.sql
+++ fetchSystem/fetchTableColonneProcedure.sql
@@ -1,34 +1,34 @@
 BEGIN TRAN
 
  
-DECLARE @nt VARCHAR(50) = '%N_A_S%'
+DECLARE @nt VARCHAR(50) = '%cours%'
 
---SELECT  DISTINCT(p.objName) AS procedureStocke
---		,t.name AS nomTable 		
--- 		,c.name AS nomColone
---		,p.def
+SELECT  DISTINCT(p.objName) AS procedureStocke
+		,t.name AS nomTable 		
+ 		,c.name AS nomColone
+		,p.def
 		
---FROM    sys.tables AS t
+FROM    sys.tables AS t
 	
---INNER JOIN sys.all_columns AS c
---	ON t.object_id   = c.object_id 
+INNER JOIN sys.all_columns AS c
+	ON t.object_id   = c.object_id 
  
  
---INNER JOIN ( SELECT DISTINCT OBJECT_NAME(OBJECT_ID) AS objName
---					,OBJECT_ID
---					,object_definition(OBJECT_ID) AS def
+INNER JOIN ( SELECT DISTINCT OBJECT_NAME(OBJECT_ID) AS objName
+					,OBJECT_ID
+					,object_definition(OBJECT_ID) AS def
  
---			FROM sys.Procedures  
---			WHERE is_ms_shipped = 0  
---			) AS p 
---			ON  object_definition(p.OBJECT_ID) LIKE '%' + c.name + '%'
---			AND object_definition(p.OBJECT_ID) LIKE '%' + t.name + '%'
+			FROM sys.Procedures  
+			WHERE is_ms_shipped = 0  
+			) AS p 
+			ON  object_definition(p.OBJECT_ID) LIKE '%' + c.name + '%'
+			AND object_definition(p.OBJECT_ID) LIKE '%' + t.name + '%'
 
---WHERE p.def like  @nt -- t.name = @nt
+WHERE p.def like  @nt -- t.name = @nt
 
---ORDER BY p.objName
---		,t.name
---		,c.name
+ORDER BY p.objName
+		,t.name
+		,c.name
 		
 
 
@@ -36,11 +36,11 @@ SELECT DISTINCT OBJECT_NAME(OBJECT_ID) AS objName
 					,OBJECT_ID
 					,object_definition(OBJECT_ID) AS def
  
-			FROM sys.Procedures  
-			WHERE is_ms_shipped = 0  
+FROM sys.Procedures  
+WHERE is_ms_shipped = 0  
 
 
-			and object_definition(OBJECT_ID)  like  @nt -- t.name = @nt
+and object_definition(OBJECT_ID)  like  @nt -- t.name = @nt
   
 
 	 	  
